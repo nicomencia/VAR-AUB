0be29c07a938affd28c743b1c5d87641346f791b
nicomencia <nicomencia4@gmail.com>
2021-03-25 19:22:37 +0100

    Add C# Thingspeak retrieve data

---
+++ Unity/Thingspeak data script/Thingspeak_data_retrieval/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Thingspeak_data_retrieval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient webClient = new WebClient();
+            var data = webClient.DownloadString("http://api.thingspeak.com/channels/1326912/feed.json?results=1");
+            JObject o = JObject.Parse(data);
+            Console.WriteLine(o["feeds"][0]["field1"]);
+            Console.ReadLine();
+        }
+    }
+}
